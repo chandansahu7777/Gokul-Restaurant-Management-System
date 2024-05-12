225f7147300ee16835b1506570b0230d69c12f8c
Chandan Kumar Sahu <135510945+chandansahu7777@users.noreply.github.com>
2024-05-13 00:22:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+var x = document.getElementById("login");
+var y = document.getElementById("register");
+var z = document.getElementById("btn");
+
+function register() {
+    x.style.left = "-400px";
+    y.style.left = "50px";
+    z.style.left = "110px";
+}
+
+function login() {
+    x.style.left = "50px";
+    y.style.left = "450px";
+    z.style.left = "0px";
+}
+
+function message() {
+    alert("Item Added To Cart");
+}
+
+var navLinks = document.getElementById("navLinks");
+
+function showmenu() {
+    navLinks.style.right = "0";
+}
+
+function hidemenu() {
+    navLinks.style.right = "-200px";
+}
